eb860d3047464b81d11ce041dcaf1610b34343e7
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-06-04 20:24:25 +0200

    se crean los botones para el filter - filtran por categoria en la consola

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,30 @@
 //DEBE imprimir en pantalla la información de filtros.
 
+import { filters, products } from "../assets/data/data.js";
+
+let filterContainer = document.querySelector(".filters-container");
+
+for (let index = 0; index < filters.length; index++) {
+  const btn = document.createElement("button");
+  btn.classList.add("filter");
+  btn.innerHTML = filters[index];
+
+  filterContainer.appendChild(btn);
+}
+
+// Obtén una referencia a los botones filtradores
+const filterButtons = document.querySelectorAll(".filter");
+
+// Agrega un evento de clic a cada botón
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const category = button.innerHTML.toLowerCase();
+    const filteredProducts = products.filter(
+      (product) => product.category === category
+    );
+    console.log(filteredProducts);
+  });
+});
+
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
