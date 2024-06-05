7010de323da3db8e12ff29c8037dd2a6e34a2070
Angelica perez <angelicanakaridperezpino@gmail.com>
2024-06-05 14:20:07 +0200

    displayMenu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,33 @@
+import { products } from "../assets/data/data.js";
 //DEBE imprimir en pantalla la información de filtros.
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
+function displayMenu(products) {
+    const productsContainer = document.querySelector('#products');
+    products.forEach(product => {
+        const productContainer = document.createElement('div');
+        productContainer.classList.add('product-container');
+        productContainer.innerHTML = `
+                        <h3>${product.name}</h3>
+                        <p>${product.description}</p>
+                        <div class="price-container">
+                            <h5>${product.price} €</h5>
+                            <button class="add-button" id=${product.id}>Añadir</button>
+                        </div>
+                        `
+
+        productsContainer.appendChild(productContainer);
+    })
+
+}
+displayMenu(products)
+
+
+
+
+
+
+
+
+
