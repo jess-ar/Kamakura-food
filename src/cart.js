2877ff5cb02934549fd7416725f610b266ba34f9
Valentina Toni <vale.toni93@gmail.com>
2024-06-05 12:20:49 +0200

    added function add product

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,23 @@
 //DEBE contener las funcionalidades del carrito de compras.
 
+const cart = [];
+
+
+function addToCart (product){
+    product.quantity = 1;
+    cart.push(product);
+}
+
+function addSameProduct (product){
+    const index = cart.findIndex(item => item.id === product.id)
+    if (index !== -1) {
+        product.quantity += 1;
+    } else { 
+        addToCart();
+    }
+    
+}
+
+
+
+
