b4da711b6d31da22c7fa81a16aaa570559e4f094
jess-ar <jessica18_24@hotmail.com>
2024-06-06 14:55:23 +0200

    Implement functionality to open receipt

--- src/cart.js
+++ src/cart.js
@@ -74,4 +74,15 @@ function displayCart() {
 }
 
 
-export { addToCart }
+//SHOW-RECEIPT
+function getCartItems() { //con esto obtengo los articulos del carrito
+    return cart;
+}
+
+function getCartTotal() { //con esto tengo el total del carrito
+    return cart.reduce((total, product) => total + product.price * product.quantity, 0);
+}
+
+export { addToCart, getCartItems, getCartTotal }; //AÑADIDO  getCartItems, getCartTotal
+
+
--- src/events.js
+++ src/events.js
@@ -1,6 +1,7 @@
 //Intenta separar los eventos en este archivo.
 import { products } from '../assets/data/data.js';
 import { addToCart } from './cart.js';
+import { showReceipt, closeReceipt } from './receipt.js';
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -35,3 +36,25 @@ document.addEventListener('DOMContentLoaded', function () {
     // });
 
 });
+
+
+// SHOW RECEIPT
+// con esto obtengo el boton de proceder al pago
+const proceedPayButton = document.getElementById('proceedPay-button');
+proceedPayButton.addEventListener('click', function () {   
+    const receiptContainer = document.getElementById('receipt-container');  //obtengo el contededor del recibo
+    const productsContainer = document.getElementById('products-container');  //obtengo el contenedor del productos del carrito
+    productsContainer.appendChild(receiptContainer); // Inserta el contenedor del recibo dentro del contenedor de productos
+    const cartProducts = document.getElementById('cart-products');
+    cartProducts.style.display = 'none';   // Oculta los productos del carrito
+    showReceipt();  // Muestra el recibo
+});
+
+// CLOSE RECEIPT
+const closeReceiptButton = document.getElementById('close-receipt'); // Obtiene el botón de cerrar recibo
+closeReceiptButton.addEventListener('click', function () {
+    const cartProducts = document.getElementById('cart-products'); // Obtiene el contenedor de los productos del carrito
+    cartProducts.style.display = 'block'; // Muestra los productos del carrito nuevamente
+    closeReceipt(); // Cierra el recibo
+});
+
